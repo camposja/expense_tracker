77c99e6daf8a6670eedee4d622ac4958f2f0eb27
Jose Campos <jose.campos@kin.com>
2022-09-25 22:53:54 -0400

    up to handling success, page 70

--- app/api.rb
+++ app/api.rb
@@ -3,12 +3,18 @@ require 'json'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    def initialize(ledger: Ledger.new)
+      @ledger = ledger
+    end
+
     post '/expenses' do 
       JSON.generate('expense_id' => 42)
     end
 
-    get '/expenses/:date' do 
-      JSON.generate([])
+    get '/expenses/:date' do
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+      JSON.generate('expense_id' => result.expense_id)
     end
   end
 end
